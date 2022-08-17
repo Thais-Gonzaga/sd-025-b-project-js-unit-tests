54df6d00ab7c85140faee382f2463efece4264a0
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-16 23:39:24 -0300

    feat: implementada uma função vqv que retorna uma string;
    ref# requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,10 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
+const vqv = (name, age) => name && age 
+&& `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 
 module.exports = vqv;
