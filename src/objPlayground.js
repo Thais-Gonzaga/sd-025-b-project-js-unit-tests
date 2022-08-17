4491f81d9bc4925c22f2f2a845490058f4574207
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-17 16:34:46 -0300

    feat: desenvolve funções calculator e arrayGenerator
    ref# requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,9 +33,21 @@
   arrayGenerator('values', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ 3, 2, 0, -1 ]
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
+// Math.floor retorna o menor número inteiro
+const calculator = (number1, number2) => { 
+  const result = {
+  sum: number1 + number2, 
+  mult: number1 * number2, 
+  div: Math.floor(number1 / number2),
+  sub: number1 - number2,
+};
+return result;
+};
 
-const calculator = (number1, number2) => {};
-
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  if (type === 'keys') return Object.keys(object);
+  if (type === 'values') return Object.values(object);
+  if (type === 'entries') return Object.entries(object);
+};
 
 module.exports = { calculator, arrayGenerator };
