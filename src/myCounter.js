f7397e9f3c319377a59f8c8f4406632e123c17ff
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-17 16:56:21 -0300

    feat: implementada uma função myCounter
    ref# requisito 8;

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,13 +14,14 @@
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
+// var escapa do escopo
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
